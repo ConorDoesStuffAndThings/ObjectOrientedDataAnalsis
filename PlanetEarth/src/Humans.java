c3a0e5a501f1b1c2a25292d9f37a63a56e034612
Conor H <conorhartigan13@gmail.com>
2020-09-28 18:26:20 +0100

    Add files via upload

---
+++ PlanetEarth/src/Humans.java
@@ -0,0 +1,56 @@
+public class Humans {
+    private String name;
+    private int age;
+    private double height;
+    private String eyeColour;
+
+    public Humans(String name, int age, double height, String eyeColour) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.eyeColour = eyeColour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getEyeColour() {
+        return eyeColour;
+    }
+
+    public void setEyeColour(String eyeColour) {
+        this.eyeColour = eyeColour;
+    }
+
+    public void Speak(String words){
+
+    }
+
+    public void Eat(String food){
+    }
+    public void Walk(String walking){
+
+    }
+
+}
